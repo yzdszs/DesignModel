40b24144fe3ce997b93f1f9c2fdd580862659d2d
persimmon <819910532>
2022-11-04 16:36:15 +0800

    Initial commit

---
+++ src/test/java/com/persimmon/designmodel/DesignModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.persimmon.designmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
