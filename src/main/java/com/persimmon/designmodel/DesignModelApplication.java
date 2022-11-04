1817f61e1f0e60bb21c1d560c39efeedfee15402
persimmon <819910532>
2022-11-04 16:38:49 +0800

    初始化项目

--- src/main/java/com/persimmon/designmodel/DesignModelApplication.java
+++ src/main/java/com/persimmon/designmodel/DesignModelApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DesignModelApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DesignModelApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DesignModelApplication.class, args);
+    }
 
 }
